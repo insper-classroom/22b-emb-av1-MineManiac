588431552e7c3dd7268d75b88e47b2d5c0eb8944
MineManiac <matheusbarrosr@gmail.com>
2022-10-03 18:06:24 -0300

    push codigo que fez meu pc crashar

--- RTOS-OLED-Xplained-Pro/src/main.c
+++ RTOS-OLED-Xplained-Pro/src/main.c
@@ -5,15 +5,56 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
-/* Botao da placa */
-#define BUT_PIO     PIOA
-#define BUT_PIO_ID  ID_PIOA
-#define BUT_PIO_PIN 11
-#define BUT_PIO_PIN_MASK (1 << BUT_PIO_PIN)
+/* Botao da placa 1*/
+#define BUT1_PIO     PIOD
+#define BUT1_PIO_ID  ID_PIOD
+#define BUT1_PIO_PIN 28
+#define BUT1_PIO_PIN_MASK (1 << BUT1_PIO_PIN)
+
+/* Botao da placa 2*/
+#define BUT2_PIO     PIOC
+#define BUT2_PIO_ID  ID_PIOC
+#define BUT2_PIO_PIN 31
+#define BUT2_PIO_PIN_MASK (1 << BUT2_PIO_PIN)
+
+/* Botao da placa 3*/
+#define BUT3_PIO     PIOA
+#define BUT3_PIO_ID  ID_PIOA
+#define BUT3_PIO_PIN 19
+#define BUT3_PIO_PIN_MASK (1 << BUT3_PIO_PIN)
+
+/* Fase 0*/
+#define FASE0_PIO     PIOD
+#define FASE0_PIO_ID  ID_PIOD
+#define FASE0_PIO_PIN 30
+#define FASE0_PIO_PIN_MASK (1 << FASE0_PIO_PIN)
+
+/* Fase 1*/
+#define FASE1_PIO     PIOA
+#define FASE1_PIO_ID  ID_PIOA
+#define FASE1_PIO_PIN 6
+#define FASE1_PIO_PIN_MASK (1 << FASE1_PIO_PIN)
+
+/* Fase 2*/
+#define FASE2_PIO     PIOC
+#define FASE2_PIO_ID  ID_PIOC
+#define FASE2_PIO_PIN 19
+#define FASE2_PIO_PIN_MASK (1 << FASE2_PIO_PIN)
+
+/* Fase 3*/
+#define FASE3_PIO     PIOA
+#define FASE3_PIO_ID  ID_PIOA
+#define FASE3_PIO_PIN 2
+#define FASE3_PIO_PIN_MASK (1 << FASE3_PIO_PIN)
+
 
 /** RTOS  */
-#define TASK_OLED_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
-#define TASK_OLED_STACK_PRIORITY            (tskIDLE_PRIORITY)
+#define TASK_MODO_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
+#define TASK_MODO_STACK_PRIORITY            (tskIDLE_PRIORITY)
+
+#define TASK_MOTOR_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
+#define TASK_MOTOR_STACK_PRIORITY            (tskIDLE_PRIORITY)
+
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,  signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
@@ -22,8 +63,11 @@ extern void vApplicationMallocFailedHook(void);
 extern void xPortSysTickHandler(void);
 
 /** prototypes */
-void but_callback(void);
+void but1_callback(void);
+void but2_callback(void);
+void but3_callback(void);
 static void BUT_init(void);
+void FASE_init(void);
 
 /************************************************************************/
 /* RTOS application funcs                                               */
@@ -40,26 +84,75 @@ extern void vApplicationTickHook(void) { }
 
 extern void vApplicationMallocFailedHook(void) {
 	configASSERT( ( volatile void * ) NULL );
+	
 }
+
+/************************************************************************/
+/* recursos RTOS                                                        */
+/************************************************************************/
+
+/** Queue for msg log send data */
+QueueHandle_t xQueueModo;
+QueueHandle_t xQueueSteps;
 
 /************************************************************************/
 /* handlers / callbacks                                                 */
 /************************************************************************/
 
-void but_callback(void) {
+void but1_callback(void) {
+	int angulo = 45;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	xQueueSendFromISR(xQueueModo, (void *) &angulo,&xHigherPriorityTaskWoken);
+}
+
+void but2_callback(void) {
+	int angulo = 90;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	xQueueSendFromISR(xQueueModo, (void *) &angulo,&xHigherPriorityTaskWoken);
+}
+
+void but3_callback(void) {
+	int angulo = 180;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	xQueueSendFromISR(xQueueModo, (void *) &angulo,&xHigherPriorityTaskWoken);
 }
 
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
 
-static void task_oled(void *pvParameters) {
+static void task_modo(void *pvParameters) {
+	/* iniciliza botoes */
+	BUT_init();
+	
+	uint32_t graus = 0;
+	
+	uint32_t steps = 0;
+	
 	gfx_mono_ssd1306_init();
-  gfx_mono_draw_string("Exemplo RTOS", 0, 0, &sysfont);
-  gfx_mono_draw_string("oii", 0, 20, &sysfont);
+	
 
 	for (;;)  {
-    
+		if (xQueueReceive(xQueueModo, &graus, (TickType_t) 0)) {
+			/* atualiza frequencia */
+			steps = graus / 0.17578125;
+			
+			/* envia nova frequencia para a task_led */
+			xQueueSend(xQueueSteps, (void *)&steps, 5);
+				
+			printf("MODO: %d \n", graus);
+			gfx_mono_draw_string("MODO", 0, 0, &sysfont);
+			sprintf(graus, "%d", graus);
+			gfx_mono_draw_string(graus, 0, 20, &sysfont);
+		}
+	}
+}
+
+static void task_motor(void *pvParameters) {
+	
+
+	for (;;)  {
+		
 
 	}
 }
@@ -85,15 +178,49 @@ static void configure_console(void) {
 
 static void BUT_init(void) {
 	/* configura prioridae */
-	NVIC_EnableIRQ(BUT_PIO_ID);
-	NVIC_SetPriority(BUT_PIO_ID, 4);
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 4);
 
 	/* conf botão como entrada */
-	pio_configure(BUT_PIO, PIO_INPUT, BUT_PIO_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
-	pio_set_debounce_filter(BUT_PIO, BUT_PIO_PIN_MASK, 60);
-	pio_enable_interrupt(BUT_PIO, BUT_PIO_PIN_MASK);
-	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_PIN_MASK, PIO_IT_FALL_EDGE , but_callback);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT1_PIO, BUT1_PIO_PIN_MASK, 60);
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_PIN_MASK);
+	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_PIO_PIN_MASK, PIO_IT_FALL_EDGE , but1_callback);
+	
+	/* configura prioridae */
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 4);
+
+	/* conf botão como entrada */
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_PIO_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT2_PIO, BUT2_PIO_PIN_MASK, 60);
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_PIN_MASK);
+	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIO_PIN_MASK, PIO_IT_FALL_EDGE , but2_callback);
+	
+	/* configura prioridae */
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 4);
+
+	/* conf botão como entrada */
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_PIO_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT3_PIO, BUT3_PIO_PIN_MASK, 60);
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_PIN_MASK);
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_PIN_MASK, PIO_IT_FALL_EDGE , but3_callback);
 }
+
+void FASE_init(){
+	pmc_enable_periph_clk(FASE0_PIO_ID);
+	pio_set_output(FASE0_PIO, FASE0_PIO_PIN_MASK, 1, 0, 0);
+
+	pmc_enable_periph_clk(FASE1_PIO_ID);
+	pio_set_output(FASE1_PIO, FASE1_PIO_PIN_MASK, 1, 0, 0);
+	
+	pmc_enable_periph_clk(FASE2_PIO_ID);
+	pio_set_output(FASE2_PIO, FASE2_PIO_PIN_MASK, 1, 0, 0);
+	
+	pmc_enable_periph_clk(FASE3_PIO_ID);
+	pio_set_output(FASE3_PIO, FASE3_PIO_PIN_MASK, 1, 0, 0);
+};
 
 /************************************************************************/
 /* main                                                                 */
@@ -107,10 +234,31 @@ int main(void) {
 
 	/* Initialize the console uart */
 	configure_console();
+	
+	/* cria queue com 32 "espacos" */
+	/* cada espaço possui o tamanho de um inteiro*/
+	xQueueModo = xQueueCreate(32, sizeof(uint32_t));
+	xQueueSteps = xQueueCreate(32, sizeof(uint32_t));
+	
+	if (xQueueModo == NULL){
+		printf("falha em criar a queue Modo \n");
+	}
+	if (xQueueSteps == NULL){
+		printf("falha em criar a queue Steps \n");
+	}
+
 
 	/* Create task to control oled */
-	if (xTaskCreate(task_oled, "oled", TASK_OLED_STACK_SIZE, NULL, TASK_OLED_STACK_PRIORITY, NULL) != pdPASS) {
-	  printf("Failed to create oled task\r\n");
+	if (xTaskCreate(task_modo, "modo", TASK_MODO_STACK_SIZE, NULL, TASK_MODO_STACK_PRIORITY, NULL) != pdPASS) {
+	  printf("Failed to create modo task\r\n");
+	} else {
+	printf("task modo created \r\n");
+	}
+	
+	if (xTaskCreate(task_modo, "motor", TASK_MODO_STACK_SIZE, NULL, TASK_MODO_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create motor task\r\n");
+		} else {
+		printf("task motor created \r\n");
 	}
 
 	/* Start the scheduler. */
